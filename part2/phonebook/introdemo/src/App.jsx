3366e76cbf894f8d1b6ec12282c488584f5864b8
Fahofah <fahriulucaycy@gmail.com>
2025-06-22 11:27:49 +0100

    part2 ex2.11

--- part2/phonebook/introdemo/src/App.jsx
+++ part2/phonebook/introdemo/src/App.jsx
@@ -1,18 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from './components/Persons'
 import NewPersonForm from './components/NewPersonForm'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  const personsUrl = 'http://localhost:3001/persons'
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get(personsUrl)
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+    }, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
